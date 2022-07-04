1a53cd16e816ad040e8f2afdb632c07674d91caa
Martin Pavlas <martin@think-forth.com>
2022-07-04 18:18:23 +0200

    Improve visual style of ActionButton

--- src/Components/ActionButton.h
+++ src/Components/ActionButton.h
@@ -10,4 +10,34 @@ public:
     }
 
     virtual ~ActionButton() = default;
+
+    void paint(Graphics &g) override
+    {
+        uint16_t backgroundColour = getColour();
+        uint16_t radius = getRadius();
+        bool highlighted = isHighlighted();
+        bool selected = isSelected();
+
+        if (selected) {
+            backgroundColour = getSelectedColour();
+        }
+
+        if (highlighted) {
+            backgroundColour = Colours::lighter(backgroundColour, 0.1f);
+        }
+
+        g.setColour(Colours::darker(backgroundColour, 0.5f));
+        g.fillRoundRect(0, 0, getWidth(), getHeight(), radius);
+        g.setColour(backgroundColour);
+        g.fillRoundRect(0, 0, getWidth(), getHeight() - 4, radius);
+        g.printText(0,
+                    getHeight() / 2 - 7,
+                    getLabel(),
+                    TextStyle::smallTransparent,
+                    getWidth(),
+                    TextAlign::center);
+        if (dimmed == true) {
+            g.dimAll();
+        }
+    }
 };
