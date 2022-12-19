8d48c15d06098328e8a0a6e4db2d3c916cd81584
Martin Pavlas <martin@think-forth.com>
2022-12-19 19:39:40 +0100

    Mark as version 3.0

--- src/ControllerApp.h
+++ src/ControllerApp.h
@@ -33,7 +33,7 @@ public:
 
     const char *getApplicationVersion(void) const override
     {
-        return ("3.0-a.29");
+        return ("3.0");
     }
 
     const char *getApplicationSandbox(void) const override
