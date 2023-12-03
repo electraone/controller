0b701023a8e01d10bae6d0abd0cbc709b06e55ba
Martin Pavlas <martin@electra.one>
2023-12-03 16:15:58 +0100

    Mark as version 3.5.2

--- src/ControllerApp.h
+++ src/ControllerApp.h
@@ -33,7 +33,7 @@ public:
 
     const char *getApplicationVersion(void) const override
     {
-        return ("3.5.1");
+        return ("3.5.2");
     }
 
     const char *getApplicationSandbox(void) const override
